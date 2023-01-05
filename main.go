c46bd2f18a516e14e077270dce42e27c585f4345
Tommy Trøen <tommy.troen@nav.no>
2023-01-05 11:57:21 +0100

    feat: use default sync duration (10hrs), cleanup comments
    Co-authored-by: sindrerh2 <sindre.rodseth.hansen@nav.no>
    Co-authored-by: jhrv <johnny.horvi@nav.no>
    Co-authored-by: muni10 <sten.ivar.rokke@nav.no>

--- main.go
+++ main.go
@@ -19,8 +19,6 @@ package main
 import (
 	"flag"
 	"os"
-	"time"
-
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -75,7 +73,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	syncDuration := 10 * time.Second
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
@@ -83,18 +80,6 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "9046ff70.nais.io",
-		SyncPeriod:             &syncDuration,
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
