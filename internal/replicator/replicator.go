27a27fba9095f0499dd20e6e1d417c7a3ae453d0
Tommy Trøen <tommy.troen@nav.no>
2023-01-06 13:47:16 +0100

    feat(values): normalize keys in templatevalues for use in templating
    * e.g. app.kubernetes.io/instance will result in a key with name instance
    
    Co-authored-by: sindrerh2 <sindre.rodseth.hansen@nav.no>

--- internal/replicator/replicator.go
+++ internal/replicator/replicator.go
@@ -49,10 +49,20 @@ func filter(m map[string]string, keys []string) map[string]string {
 	values := make(map[string]string)
 	for _, k := range keys {
 		if v, ok := m[k]; ok {
-			values[k] = v
+			values[normalizeKey(k)] = v
 		}
 	}
 	return values
+}
+
+// TODO: discuss how to handle annotations with url syntax - e.g. app.kubernetes.io/instance
+// should we get value after last slash (possible collisions with other annotations)?
+// or should we replace all . and slashes with _ (worse user experience with regards to templating)?
+func normalizeKey(key string) string {
+	if strings.Contains(key, "/") {
+		return strings.Split(key, "/")[1]
+	}
+	return key
 }
 
 func extractDefaultAnnotations(annotations map[string]string) map[string]string {
--- internal/replicator/replicator_test.go
+++ internal/replicator/replicator_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	naisiov1 "nais/replicator/api/v1"
 
 	"github.com/stretchr/testify/assert"
@@ -28,4 +30,33 @@ func TestResources(t *testing.T) {
 	assert.NoError(t, err)
 	fmt.Printf("resources: %v\n", resources[0].Object["data"])
 
+}
+
+func TestExtractValues(t *testing.T) {
+	ns := v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+			Annotations: map[string]string{
+				"foo":             "bar",
+				"some.url.io/key": "annotation_value",
+			},
+			Labels: map[string]string{
+				"baz": "qux",
+			},
+		},
+	}
+
+	nsValues := naisiov1.Namespace{
+		Annotations: []string{"foo", "some.url.io/key"},
+		Labels:      []string{"baz"},
+	}
+
+	values := ExtractValues(ns, nsValues)
+
+	fmt.Printf("values: %v\n", values)
+
+	assert.Equal(t, "bar", values["foo"])
+	assert.Equal(t, "qux", values["baz"])
+	assert.Equal(t, "", values["some.url.io/key"])
+	assert.Equal(t, "annotation_value", values["key"])
 }
