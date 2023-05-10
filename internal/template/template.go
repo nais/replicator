ac8249e269bfb3cca11f3bafe991cca40a96c3f2
Erik Vattekar <erik.vattekar@nav.no>
2023-05-10 18:40:26 +0200

    add b64enc function to handle multiline strings
    template values can optionally be base64 encoded with
    ...
    spec:
      data:
        value: [[ index .Values "tls.crt" | b64enc ]]

--- internal/template/template.go
+++ internal/template/template.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"text/template"
 
+	b64 "encoding/base64"
+
 	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -56,7 +58,9 @@ func repairMapAny(v any) any {
 }
 
 func renderString(values any, tpl string, tplOptions ...RenderOption) (string, error) {
-	t := template.New("tpl").Delims("[[", "]]")
+	t := template.New("tpl").Delims("[[", "]]").Funcs(template.FuncMap{
+		"b64enc": b64enc,
+	})
 	for _, option := range tplOptions {
 		t = option(t)
 	}
@@ -70,4 +74,8 @@ func renderString(values any, tpl string, tplOptions ...RenderOption) (string, e
 		return "", err
 	}
 	return buf.String(), nil
+}
+
+func b64enc(s string) string {
+	return b64.StdEncoding.EncodeToString([]byte(s))
 }
