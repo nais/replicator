90eaf211b922cf5ad4973e3529d06d0df0048a37
Kyrre Havik <kyrre.havik@nav.no>
2026-06-03 13:29:11 +0200

    fix: manglet groupversion

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -20,6 +20,7 @@ limitations under the License.
 package v1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -40,6 +41,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ReplicationConfig{},
 		&ReplicationConfigList{},
 	)
+
+	metav1.AddToGroupVersion(scheme, GroupVersion)
 
 	return nil
 }
